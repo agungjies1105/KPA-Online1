2c11c295ef8b49b85b4252d3db3a17e77b25bfd5
Agung Gunawan <agung.one94@gmail.com>
2020-05-05 15:37:22 +0700

    Update dari Agung
    Saya menambahkan Jumlah.php

---
+++ Jumlah.php
@@ -0,0 +1,18 @@
+<?php 
+   $data1= $_POST['data1'];
+   //Operasi Penjumlahan
+   $devfaren=32;
+   $total = ($data1*1.8) + $devfaren;
+   echo $total; echo "F";
+
+   if ($total==93.2) {
+   	echo "Anda Sehat...!";
+   } elseif ($total<93.2) {
+   	echo "Anda Kedinginan";
+   } elseif ($total>100) {
+   	echo "Anda Sakit Parah";
+   } else {
+   	echo "Anda Sakit";
+   }
+
+?>
